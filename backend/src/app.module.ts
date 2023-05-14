84c6d980c9095aef17e8a1b3ebc6a6154c92323f
valuedriven <junilson@hotmail.com>
2023-05-13 22:07:21 -0300

    Ajuste nas configurações - settings

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import appConfig from './config/config';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'sqlite',
-      omitNull: true,
-      autoLoadModels: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      load: [appConfig],
+      isGlobal: true,
+    }),
+    SequelizeModule.forRootAsync({
+      useFactory: (config: ConfigService) => config.get('database'),
+      inject: [ConfigService],
     }),
     ProductsModule,
   ],
---
+++ backend/src/config/config.ts
@@ -0,0 +1,18 @@
+export default () => ({
+  port: process.env.PORT,
+  database: {
+    dialect: process.env.DB_DIALECT || 'sqlite',
+    host: process.env.DB_HOSTNAME,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    storage: process.env.DB_STORAGE,
+    omitNull: process.env.DB_OMITNULL || true,
+    autoLoadModels: process.env.DB_AUTOLOADMODELS || true,
+    synchronize: process.env.DB_SYNCRONIZE || true,
+    define: {
+      timestamps: process.env.DB_TIMESTAMPS || true,
+    },
+  },
+});
