83b13932c3abe521536a52e477b4a35a3cb636d8
JPSouza <junilson@hotmail.com>
2023-05-25 21:44:26 -0300

    Update app.service.ts

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to the Conchayoro Backend - v22!';
+    return 'Welcome to the Conchayoro Backend - v3!';
   }
 }
